379203174c77dd3bdcdea9e699807be3a7ea2b76
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-12-16 18:47:31 -0800

    wip

--- src/Alertmanager.Receiver.AzureDevOps/Settings.cs
+++ src/Alertmanager.Receiver.AzureDevOps/Settings.cs
@@ -4,9 +4,9 @@ public class Settings
 {
     public LogLevel LogLevel { get; set; }
 
-    public string PAT { get; set; } = null!;
-
     public string Organization { get; set; } = null!;
 
     public string Project { get; set; } = null!;
+
+    public string PAT { get; set; } = null!;
 }
